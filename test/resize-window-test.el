d23e994398a1b75ada36c285834ff9b77aec8259
dan <dan@bigmac.local>
2015-11-23 20:33:29 -0600

    tests added for match capitals, execute action and others

--- test/resize-window-test.el
+++ test/resize-window-test.el
@@ -5,6 +5,32 @@
 
 ;;; Code:
 
-(ert-deftest resize-window-should-not-pass ()
-  (should-not nil))
+(defvar choice-no-capital '(?n 'function "documentation" nil))
+(defvar choice-capital '(?n 'function "documentation" t))
 
+(ert-deftest should-match-aliases ()
+  (should (equal ?f
+                 (rw-match-alias 'right)))
+  (should (equal ?b
+                 (rw-match-alias 'left)))
+  (should (equal ?n
+                 (rw-match-alias 'up)))
+  (should (equal ?p
+                 (rw-match-alias 'down))))
+
+(ert-deftest should-create-documentation-from-alist ()
+  (should (equal "n: documentation "
+                 (rw-display-choice choice-no-capital)))
+  (should (equal "n|N: documentation "
+                 (rw-display-choice choice-capital))))
+
+(ert-deftest should-execute-and-display-message ()
+  (let ((choice '(?n (lambda () (setq executed t)) "doc" nil))
+        (executed)
+        (message-received ""))
+    (rw-execute-action choice)
+    (should (equal executed t))))
+
+(ert-deftest should-identify-which-allow-capital-matching ()
+  (should (rw-allows-capitals choice-capital))
+  (should-not (rw-allows-capitals choice-no-capital)))
